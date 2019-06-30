3bbdb4b73700cd22cf8afeed8d21b7972c5c76e5
mastercoder <48371762+revolution586@users.noreply.github.com>
2019-06-29 22:38:41 -0400

    Update HighScore.cs

--- HighScore.cs
+++ HighScore.cs
@@ -1,1 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Pacman
+{
+	public class HighScore
+	{
+			public const int InitialScore = 100;
+			public Label HighScoreText = new Label();
+			public int Score = InitialScore;
+			
+			public void CreateHighScore(Form formInstance)
+			{
+				// Create Score label
+				HighScoreText.FontColor
+				HighScoreText.Font
+				HighScoreText.Top
+				HighScoreText.Left
+				HighScoreText.Height
+				HighScoreText.Width
+				formInstance.Controls.Add(HighScoreText);
+				HighScoreText.BringToFront();
+				UpdateHighScore();
+			}
+			
+			public void UpdateHighScore(int value = InitialScore)
+			{
+				Score = value;
+				HighScoreText.Text = Score.ToString()'
+			}
+		}
+	}
+				
+				
 
