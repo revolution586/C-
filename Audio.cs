4abddead48027edbb9a10e1f01d898c0a26e310f
mastercoder <48371762+revolution586@users.noreply.github.com>
2019-07-02 17:15:18 -0700

    Update Audio.cs

--- Audio.cs
+++ Audio.cs
@@ -6,5 +6,33 @@ using System.Media;
 
 namespace Pacman
 {
+	public class Audio
+	{
+		private const int players = 20;
+		private SoundPlayer[] player = new SoundPlayer[players];
+		
+		public Audio()
+		{
+			for (int x=0; x<players; x++)
+			{
+				player[x] = new SoundPlayer();
+				player[x].Stream = Properties.Resources.audio1;
+			}
+		}
+		
+		public void Player(int num)
+		{
+			// int thisone = 0;
+			// for (int x=0; x<players; x++)
+			// {
+			//	if (player[x].IsLoadCompleted) { thisone = x; }
+			// }
+			// player[num].LoadAsync();
+			player[num].Play();
+		}
+	}
+}
+		
+		
 	
   
